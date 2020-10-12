b8e0333cf4e1e6c83bf5c55faaf8829a7418f7ce
Marcus Jensen <marcusjensen1515@gmail.com>
2020-10-12 11:04:44 -0700

    sucesfully utilizing os totalmem and os freemem out of the box functions

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //require function is unique to node, and how we import modules.
 //always upload modules as constants, not vars. so we can't overwrite them
 
+//this path object is baked into node. lets us know the path.
+
 const log = require('./logger');
 
+const path = require('path');
 
-log('sup');
+const os = require ('os');
+
+var pathObj = path.parse(__filename);
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+
+console.log('Total Memory: ' + totalMemory);
+
+console.log('Free Memory ' + freeMemory);
