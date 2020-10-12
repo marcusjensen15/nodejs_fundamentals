7476f73be11ac73e683d031019f4759076be2700
Marcus Jensen <marcusjensen1515@gmail.com>
2020-10-12 11:10:37 -0700

    add method to show all the files within folder

--- app.js
+++ app.js
@@ -3,18 +3,28 @@
 
 //this path object is baked into node. lets us know the path.
 
+//ALWAYS USE ASYNC METHODS. NODE OPERATES WITH A SINGLE THREAD.
+
 const log = require('./logger');
 
 const path = require('path');
 
 const os = require ('os');
 
-var pathObj = path.parse(__filename);
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+const files = fs.readdirSync('./');
+
+console.log(files);
 
 
-console.log('Total Memory: ' + totalMemory);
 
-console.log('Free Memory ' + freeMemory);
+// var pathObj = path.parse(__filename);
+//
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+//
+//
+// console.log('Total Memory: ' + totalMemory);
+//
+// console.log('Free Memory ' + freeMemory);
